19c4207b7bd98113dcbdab956363755519308e2f
Julianne Pereira Santos Pinheiro <jupsantos@outlook.com.br>
2021-12-13 23:02:11 -0300

    CRUD de Postagem

--- Front-End/src/app/service/auth.service.ts
+++ Front-End/src/app/service/auth.service.ts
@@ -10,9 +10,7 @@ import { UsuarioLogin } from '../model/UsuarioLogin';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { 
-
-  }
+  constructor(private http: HttpClient) {}
 
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
     return this.http.post<UsuarioLogin>('https://blogpessoalju.herokuapp.com/usuarios/logar', usuarioLogin)
@@ -20,6 +18,10 @@ export class AuthService {
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('https://blogpessoalju.herokuapp.com/usuarios/cadastrar', usuario)
+  }
+
+  getByIdUsuario(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`https://blogpessoalju.herokuapp.com/usuarios/${id}`)
   }
 
   logado(){
